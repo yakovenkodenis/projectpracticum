6796f66fbbbfd706b5c9a554ab3a88e79500cb87
Mordvinov Roman <romixmor@gmail.com>
2015-05-31 15:06:10 +0300

    Added TwoWay Binding

--- Autoschool/Query.cs
+++ Autoschool/Query.cs
@@ -1,11 +1,69 @@
 ﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Autoschool.Annotations;
+
 namespace Autoschool
 {
-    public class Query
+    public class Query : INotifyPropertyChanged
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Text { get; set; }
-        public string Autoschool { get; set; }
+        private string _id;
+        private string _name;
+        private string _text;
+        private string _autoschool;
+
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                NotifyPropertyChanged("Id");
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (value == _text) return;
+                _text = value;
+                NotifyPropertyChanged("Text");
+            }
+        }
+
+        public string Autoschool
+        {
+            get { return _autoschool; }
+            set
+            {
+                if (value == _autoschool) return;
+                _autoschool = value;
+                NotifyPropertyChanged("Autoschool");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
     }
 }
